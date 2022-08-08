118a1eec652d61d9baa874f7c6f68c1c4a6be120
Michael McDonald <mjmickd@gmail.com>
2022-08-08 00:54:06 -0700

    commit tree f17164886875fc95b099afea499973d4dd4fd04c

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1= 2
+let num2= 31
+const multiply = num1*num2 
 
-// Write your code here
+const random= Math.floor(Math.random()*45)+1
+
+let num3=16
+let num4=6
+const mod= num3%num4
+
+const max= Math.max (1,5,7,20,4)
